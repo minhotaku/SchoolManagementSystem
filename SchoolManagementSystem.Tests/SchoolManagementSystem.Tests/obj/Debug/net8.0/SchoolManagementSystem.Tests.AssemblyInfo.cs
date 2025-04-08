57b6991207c64ffc228560808e37baca5ba73150
TruongGiang1701 <hatruonggiang17012004@gmail.com>
2025-04-08 16:39:35 +0700

    add file gitgnore

--- SchoolManagementSystem.Tests/SchoolManagementSystem.Tests/obj/Debug/net8.0/SchoolManagementSystem.Tests.AssemblyInfo.cs
+++ SchoolManagementSystem.Tests/SchoolManagementSystem.Tests/obj/Debug/net8.0/SchoolManagementSystem.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagementSystem.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+792c378d698304bd24eb96353b654292db20a690")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagementSystem.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagementSystem.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
